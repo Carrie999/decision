733b243a12bb658d3b16b23458caa8e8bb3428f0
carrie <771685809@qq.com>
2024-11-17 09:29:15 +0800

    Initial Commit

---
+++ decision/decisionApp.swift
@@ -0,0 +1,17 @@
+//
+//  decisionApp.swift
+//  decision
+//
+//  Created by  玉城 on 2024/11/17.
+//
+
+import SwiftUI
+
+@main
+struct decisionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
